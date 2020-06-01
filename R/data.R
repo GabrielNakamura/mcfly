ff368c953ae569c2d809a4df2fb855705059a58a
Gabriel Nakamura <gabriel.nakamura.souza@gmail.com>
2020-06-01 19:09:31 -0300

    minor modification in parallel function

--- R/data.R
+++ R/data.R
@@ -1,36 +1,29 @@
-#' Furnariidae data set
+#' @name Furnariidae
+#' @title Furnariidae data set
 #'
-#' Data frame containing occurrence of 102 species of Furnariidae in 1350 sites.
+#' @description Data frame containing occurrence of 102 species of Furnariidae in 1350 sites.
 #' 
-#'
 #' @format A data frame with 1350 rows and 102 columns:
 #' 
-#' 
-#' 
 #' @source Maestri and Duarte 2020 \url{https://link.springer.com/chapter/10.1007/978-3-030-31167-4_6}
-"Furnariidae"
+NULL
 
-#' Phylogeny of Furnariidae species
-#'
-#' Newick object containing the phylogenetic hypothesis of 102 species of Furnariidae.
+#' @name phylo_Furnariidae
+#' @title Phylogeny of Furnariidae species
 #' 
-#'
+#' @description Newick object containing the phylogenetic hypothesis of 102 species of Furnariidae.
+#' 
 #' @format Newick containing phylogenetic relationships of 102 species of Furnariidae family.
 #' 
-#' 
-#' 
 #' @source BirTree \url{https://birdtree.org/}
-#' 
-"phylo_Furnariidae"
+NULL
 
-#' Environmental variable 
+#' @name envir
+#' @title Environmental variable 
 #'
-#' Matrix containing environmental descriptor for the sites.
+#' @description Matrix containing environmental descriptor for the sites.
 #' 
-#'
 #' @format matrix
 #' 
-#' 
-#' 
 #' @source Maestri and Duarte 2020 \url{https://link.springer.com/chapter/10.1007/978-3-030-31167-4_6}
-"envir"
+NULL
